0640b165c3d3e9cc474e4bb0cdf52c370c3ca0e8
SOunit <workbookwork7@gmail.com>
2021-10-04 11:43:31 -0700

    initial commit

---
+++ first_app/src/first_app/Main.java
@@ -0,0 +1,28 @@
+package first_app;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Java");
+
+		int num = 1;
+		int newNum = 3;
+		num = newNum;
+
+		System.out.print("num, ");
+		System.out.println(num);
+
+		double doubleNum = 10.0 / 6.0;
+		System.out.print("doubleNum, ");
+		System.out.println(doubleNum);
+
+		boolean flag = true;
+		System.out.print("flag, ");
+		System.out.println(flag);
+
+		String text = "HELLO";
+		System.out.print("text, ");
+		System.out.println(text);
+
+	}
+}
