3a2de6d46b8850049c6e33b414f83e0d1fc14826
SOUnit <workbookwork7@gmail.com>
2021-10-22 11:16:32 -0700

    add answer

---
+++ first_app/src/y2020/m10/d22/Main.java
@@ -0,0 +1,16 @@
+package y2020.m10.d22;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		BankAccount<String, String> account = new BankAccount<>("dummy", "user001");
+		System.out.println("Welcome " + account.getCustomerName());
+		System.out.println("Your ID is: " + account.getCustomerId());
+		System.out.println("\n");
+
+		account.showMenu();
+
+	}
+
+}
